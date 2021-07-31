3355c74010372c74a61dd6080be6787559b1f324
Derek Chen <derex1987@gmail.com>
2021-07-31 12:10:03 -0500

    dasdasd
    asdasdwadsdaw

--- looptest.js
+++ looptest.js
@@ -4,3 +4,18 @@ for (i = 0; i < 5; i++)
     x++;
 }
 console.log(x);
+
+
+let boo = false;
+if(!boo)
+{
+    console.log('not boo');
+}
+
+console.log('boo?',boo);
+
+let grade = 'A';
+switch(grade){
+    case 'A':
+        console.log('A!');
+}
